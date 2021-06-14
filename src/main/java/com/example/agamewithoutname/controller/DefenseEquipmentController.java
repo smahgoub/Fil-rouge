9c607efdee8d9175b950d1b8247d90f4e9b2f34c
Gilles <gilles.toni@sfr.fr>
2021-06-14 12:48:08 +0200

    Ajout de tous les controllers

--- src/main/java/com/example/agamewithoutname/controller/DefenseEquipmentController.java
+++ src/main/java/com/example/agamewithoutname/controller/DefenseEquipmentController.java
@@ -1,4 +1,38 @@
 package com.example.agamewithoutname.controller;
 
+import com.example.agamewithoutname.model.DefenseEquipment;
+import com.example.agamewithoutname.repository.DefenseEquipmentRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/defenseequipment")
 public class DefenseEquipmentController {
+
+    private DefenseEquipmentRepository defenseEquipmentRepository;
+
+    public DefenseEquipmentController(DefenseEquipmentRepository defenseEquipmentRepository) {
+        this.defenseEquipmentRepository = defenseEquipmentRepository;
+    }
+
+    @GetMapping
+    public List<DefenseEquipment>GetAllDefenseEquipment() {
+        return defenseEquipmentRepository.findAll();
+    }
+
+
+    @PostMapping
+    public void createDefenseEquipment(@RequestBody  DefenseEquipment defenseEquipmentToCreate) {
+        defenseEquipmentRepository.save(defenseEquipmentToCreate);
+    }
+
+//    @DeleteMapping
+//    public void delete(@PathVariable("identifiant") Long id){
+//        defenseEquipmentRepository.deleteById(id);
+//        System.out.println("Suppression de l'entité avec l'id :" +id);
+//    }
+
+
+
 }
--- src/main/java/com/example/agamewithoutname/controller/GameUserController.java
+++ src/main/java/com/example/agamewithoutname/controller/GameUserController.java
@@ -1,4 +1,31 @@
 package com.example.agamewithoutname.controller;
 
+import com.example.agamewithoutname.model.GameUser;
+import com.example.agamewithoutname.repository.GameUserRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/admin/gameuser")
 public class GameUserController {
+
+    private GameUserRepository gameUserRepository;
+
+    public GameUserController(GameUserRepository gameUserRepository) {
+        this.gameUserRepository = gameUserRepository;}
+
+    //Get pour récupérer le liste des users existants
+    @GetMapping
+    public List<GameUser> getAllUser() {return gameUserRepository.findAll();}
+
+    //PostMapping pour créer de nouveaux users
+    @PostMapping
+    public void createGameUser(@RequestBody GameUser gameUserToCreate) {
+        gameUserRepository.save(gameUserToCreate);
+    }
+
+    //DeleteMapping pour supprimer un user
+    //@DeleteMapping("/id")
+    //public void deleteGameUser(@PathVariable Integer id) {gameUserRepository.delete(id);}
 }
